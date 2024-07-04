94f698fef653c8b8ba6f992b7b4e92ff24ca54b8
mvpclash <roy3r91@gmail.com>
2024-07-04 15:37:44 -0600

    ejercicios en js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 (()=>{
-   const denunciasBtn = document.getElementsByTagName('button')
+   const denunciasBtn = document.getElementById('denuncias');
+   const usuarioHtml = document.getElementById('usuario');
+   const passwordHtml = document.getElementById('password');
+    
+    denunciasBtn.addEventListener('click', (evento) =>{
+        console.log(usuarioHtml.value);
+        console.log(passwordHtml.value);
+        alert('Haz hecho clic en denuncias');
+    })
+    
+
+
+
+
 }
 
 )();
